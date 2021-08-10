154902240c14ab89ea7cae37d0f5a9eb4137fa1d
Derek Thompson <derekt123@outlook.com>
2021-08-09 22:23:49 -0500

    Add files via upload

---
+++ Bluetooth_Overall_Testing/Bluetooth_Overall_Testing3/Bluetooth_Overall_Testing3/Bluetooth_Overall_Testing3.ino
@@ -0,0 +1,118 @@
+#include <SD.h> //Load SD card library
+#include<SPI.h> //Load SPI Library
+
+#include <Adafruit_GPS.h>    //Install the adafruit GPS library
+#include <SoftwareSerial.h> //Load the Software Serial library
+SoftwareSerial mySerial(3,2); //Initialize the Software Serial port
+Adafruit_GPS GPS(&mySerial); //Create the GPS Object
+
+String NMEA1; //Variable for first NMEA sentence
+String NMEA2; //Variable for second NMEA sentence
+char c; //to read characters coming from the GPS
+
+int chipSelect = 4; //chipSelect pin for the SD card Reader
+File mySensorData; //Data object you will write your sesnor data to
+int const xPin = A2;
+int const yPin = A3;
+int factor;
+
+
+
+void setup() {
+
+  //pinMode(factor, OUTPUT);
+
+  
+  Serial.begin(115200); //Turn on serial monitor
+  GPS.begin(9600); //Turn on GPS at 9600 baud
+  GPS.sendCommand("$PGCMD,33,0*6D");  //Turn off antenna update nuisance data
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //Request RMC and GGA Sentences only
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); //Set update rate to 1 hz
+  delay(1000); 
+  pinMode(10, OUTPUT); //Must declare 10 an output and reserve it to keep SD card happy
+  SD.begin(chipSelect); //Initialize the SD card reader
+  
+  if (SD.exists("NMEA.txt")) { //Delete old data files to start fresh
+    SD.remove("NMEA.txt");
+  }
+
+}
+
+void loop() {
+ factor = digitalRead(7);
+ if (factor == 1){
+  nebula();
+ }
+
+  
+  readGPS();
+
+  if(GPS.fix==1) { //Only save data if we have a fix
+  mySensorData = SD.open("NMEA.txt", FILE_WRITE); //Open file on SD card for writing
+  mySensorData.println(NMEA1); //Write first NMEA to SD card
+  mySensorData.close();  //Close the file
+  mySensorData = SD.open("NMEA.txt", FILE_WRITE); //Open file on SD card for writing
+  mySensorData.println(analogRead(xPin)); //Write first NMEA to SD card
+  mySensorData.close();  //Close the file
+  mySensorData = SD.open("NMEA.txt", FILE_WRITE); //Open file on SD card for writing
+  mySensorData.println(analogRead(yPin)); //Write first NMEA to SD card
+  mySensorData.close();  //Close the file 
+  }
+ 
+
+  
+}
+
+void nebula() {
+ factor = 0;
+ mySensorData = SD.open("NMEA.txt");
+ if(mySensorData){
+  while(mySensorData.available()){
+    Serial.write(mySensorData.read());
+  }
+ }
+ mySensorData.close();
+ //vortex();
+}
+
+//void vortex(){
+ // if(SD.exists("NMEA.txt")){
+ //   SD.remove("NMEA.txt");
+ // }
+//}
+
+void readGPS() {
+  
+  clearGPS();
+  while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  NMEA1=GPS.lastNMEA();
+  
+   while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  NMEA2=GPS.lastNMEA();
+  
+
+  
+}
+
+void clearGPS() {  //Clear old and corrupt data from serial port 
+  while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  
+  while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+   while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  
+}
---
+++ GPS_Testing/GPS_Testing.ino
@@ -0,0 +1,101 @@
+// include headers
+#include <SPI.h>
+#include <SD.h>
+#include <SoftwareSerial.h>
+
+
+
+// testing var below
+//
+
+const int xpin = A2;                  // x-axis of the accelerometer
+const int ypin = A3;                  // y-axis of accelerometer
+
+
+
+
+
+// setting up data files
+File dataFileX;
+File dataFileY;
+
+
+void setup() {
+  
+  // wait for serial port to open and connect
+  Serial.begin(9600);
+
+  
+  // slave select (SS) pin 10, selecting SD card
+  pinMode(10, OUTPUT);
+
+  // checking SD module
+  if (!SD.begin(10)){
+    Serial.println("ERROR: SD Not Initialized");
+    return;
+  }
+}
+
+
+// SAVING SENSOR DATA TO STRING
+
+void loop() {
+
+  Serial.println("Accel (X): ");
+  Serial.print(analogRead(xpin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.println("Accel (Y): ");
+  Serial.print(analogRead(ypin));
+  // print a tab between values:
+  Serial.print("\t");
+
+
+
+
+   
+  // save X-axis data
+  String dataStringX = "";
+  // selecting pin
+  int analogPinX = 7;
+  int sensorX = analogRead(analogPinX);
+  dataStringX += String(sensorX);
+
+  // save Y-axis data
+  String dataStringY = "";
+  int analogPinY = 6;
+  int sensorY = analogRead(analogPinY);
+  dataStringY += String(sensorY);
+
+ 
+
+  // ************** FILE CREATION ***************************************
+  dataFileX = SD.open("accel_x.txt", FILE_WRITE);
+  dataFileY = SD.open("accel_y.txt", FILE_WRITE);
+  // ************** FILE CREATION ***************************************
+
+
+  // ********************** DATA LOGGING ********************************
+
+  // print contents of string to file
+  if (dataFileX){
+    dataFileX.println(dataStringX);
+    dataFileX.close();
+    Serial.println(dataStringX);
+  }  
+  else {
+    Serial.println("error opening file.txt");
+  }
+  
+
+  if (dataFileY){
+    dataFileY.println(dataStringY);
+    dataFileY.close();
+    Serial.println(dataStringY);
+  }  
+  else {
+    Serial.println("error opening file.txt");
+  }
+  
+  delay(2000);  
+}
